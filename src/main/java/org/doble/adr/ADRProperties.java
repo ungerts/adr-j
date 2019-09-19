2df6305bd8b9493e8c9eecbcfe04afd86f121594
Oliver Kopp <kopp.dev@gmail.com>
2019-09-19 14:36:24 +0200

    Reformat code using IntelliJ autoformat

--- src/main/java/org/doble/adr/ADRProperties.java
+++ src/main/java/org/doble/adr/ADRProperties.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package org.doble.adr;
 
 import java.nio.file.Files;
@@ -8,21 +5,13 @@ import java.nio.file.Path;
 import java.util.Properties;
 import java.io.BufferedReader;;
 
-/**
- * @author adoble
- *
- */
-public class ADRProperties extends Properties{
-	/**
-	 * 
-	 */
+public class ADRProperties extends Properties {
+
 	public static final String defaultDocPath = "doc/adr";  //TODO is this the right place for this constant?
 	public static final String defaultTemplateName = "default_template.md";
 	public static final String defaultInitialTemplateName = "default_initial_template.md";
-	
+
 	private static final long serialVersionUID = 1L;
-	
-	
 
 
 	Environment env;
@@ -31,42 +20,41 @@ public class ADRProperties extends Properties{
 	 * @param env The environment in which the tool is run in
 	 */
 	public ADRProperties(Environment env) {
-		
+
 		this.env = env;
 	}
-	
-	/** 
-	 * Reads the .adr properties file at the root directory of the project. 
+
+	/**
+	 * Reads the .adr properties file at the root directory of the project.
 	 *
 	 * @return A Properties object with the data contained in the properties file
-	 * @throws ADRException if the properties file cannot be read 
+	 * @throws ADRException if the properties file cannot be read
 	 */
-     public void load() throws ADRException {
-		//properties = new Properties();		
-		
+	public void load() throws ADRException {
+		//properties = new Properties();
+
 		// Get the root directory by looking for an .adr directory
-		
-		Path rootPath = env.dir; 
-		
-		Path propertiesRelPath  = env.fileSystem.getPath(ADR.ADR_DIR_NAME, "adr.properties");
-		
+
+		Path rootPath = env.dir;
+
+		Path propertiesRelPath = env.fileSystem.getPath(ADR.ADR_DIR_NAME, "adr.properties");
+
 		Path propertiesPath = rootPath.resolve(propertiesRelPath);
-		
+
 		try {
 			if (Files.exists(propertiesPath)) {
 				BufferedReader propertiesReader = Files.newBufferedReader(propertiesPath);
 				load(propertiesReader);
 				propertiesReader.close();
 			} else {
-				// Set the default values. This should be stored when adr init is called, 
+				// Set the default values. This should be stored when adr init is called,
 				setProperty("docPath", defaultDocPath);
 			}
 		} catch (Exception e) {
 			throw new ADRException("FATAL: The properties file could not be read.", e);
-		} 
-		
-		
-			
+		}
+
+
 	}
 
 }
